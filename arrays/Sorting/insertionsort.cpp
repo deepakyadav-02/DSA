22c9f97dd2cd13f5cdaf9c1f054090686f469ed7
Deepak Yadav <deepakyadav101002@gmail.com>
2022-10-13 00:00:19 +0530

    selection sort updated

--- arrays/Sorting/insertionsort.cpp
+++ arrays/Sorting/insertionsort.cpp
@@ -13,23 +13,9 @@ int main()
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[i])
-            {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-            }
-        }
-    }
-
-    cout << "Sorted array is:" << endl;
+    cout << "Unsorted array:" << endl;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << endl;
     }
-    return 0;
 }
