9f652e2871778f4ec626a8437285ee17bb6bae8f
Deepak Yadav <deepakyadav101002@gmail.com>
2022-10-14 22:18:06 +0530

    Arithmatic subaray added

--- arrays/Sum_subarray.cpp
+++ arrays/Sum_subarray.cpp
@@ -16,7 +16,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
         current = 0;
-        for (int j = i; i < n; j++)
+        for (int j = i; j < n; j++)
         {
             current += arr[j];
             cout << current << endl;
